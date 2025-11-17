8944612e55d5f9d3bd22cb1acd58cc15c25f75e6
Administrador <nshayronfaquir@gmail.com>
2025-11-17 01:32:37 +0000

    atualizando a logica em funcao da main22

--- PREPTEST/src/Aluguer.java
+++ PREPTEST/src/Aluguer.java
@@ -1,10 +1,10 @@
 
-import java.util.*;
+import java.time.LocalTime;
 
-public class Aluguer {
-    private float tempoutilizacao;
-    private String data;
-    protected String HoraInicio;
+public class Aluguer extends Utilizador{
+    private int data;
+    private int dataInicio;
+    private int datafim;
 
 
     public String servicoextra(){
@@ -14,11 +14,28 @@ public class Aluguer {
     public float Preco(Utilizador funcao){
         String serv = servicoextra();
 
+        LocalTime inicio = LocalTime.now();
+        int hora = inicio.getHour();
+        int minuto = inicio.getMinute();
+        int segundo = inicio.getSecond();
+        int data = inicio.atDate();
+
+
 
 
         return 1;
 
     }
+
+    public int custo_viagem(Utilizador pessoa){
+        int saldo;
+        int data_inicial;
+        int data_fim;
+        String info = pessoa.informacao();
+        saldo =  data_fim-data_inicial
+        saldo *=
+        return saldo;
+    }/**/
 
 
 
