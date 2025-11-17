14644edd4d0d1fbbc0f49aada05656243a5bf945
Administrador <nshayronfaquir@gmail.com>
2025-11-17 00:59:59 +0000

    atualizando a logica em funcao da main

--- PREPTEST/src/Funcionario.java
+++ PREPTEST/src/Funcionario.java
@@ -10,61 +10,6 @@ public class Funcionario extends Utilizador{
         this.anoContrato = anoContrato;
     }
 
-    public String docente(){// retorna se sera docente ou nao  se for retorna a lista de faculdades e se nao for docente retorna o servico em trabalha;
-        ArrayList<String> ListaFaculdades = new ArrayList<>();
-        String servico = null;
-
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Diga sua funcao !! | 1 - Docente  | 2 - Nao Docente ");
-        String input = sc.nextLine();
-
-
-        if(input.equals("1")) { // DOCENTE
-            System.out.println("Funcionario Docente !  \n ");
-
-            boolean op = true;
-
-            while (op) { // While para preencher a Lista de Faculades
-                System.out.println("Diga as faculdades em que Leciona :  | 1 - Adicionar | 2 - Concluido |");
-                String in2 = sc.nextLine();
-                if (in2.equals("1")) {
-                    String f = sc.nextLine();
-                    ListaFaculdades.add(f);
-
-                } else if (in2.equals("2")) {
-                    op = false;
-                } else {
-                    System.out.println("Opcao Invalida . \n ");
-
-                }
-
-            }
-        } else if(input.equals("2")){ // NAO DOCENTE
-
-
-                System.out.println("Funcionario Nao Docente !  \n ");
-                System.out.println("Indique o Servico onde trabalha \n ");
-                servico = sc.nextLine();
-
-            }
-        else{
-                System.out.println("Opcao Invalida . \n ");
-
-            }
-        if (input.equals("1")){
-            return "Docente nas faculdades : " + ListaFaculdades  ;
-        }
-            else if (input.equals("2")){
-                return "Funcionario em Servico : " + servico;
-        }
-            else{
-                return "Opcao Invalida . \n ";
-        }
-        }
-
-    }
     public double descontoUtilizador() {
         return 0;
     }
